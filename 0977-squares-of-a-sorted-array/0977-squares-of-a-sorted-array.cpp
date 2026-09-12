19a017bdc49daddd208d630c182ed1e7e4adb9e2
NAMAN GOEL <goelnaman366@gmail.com>
2026-09-13 02:34:54 +0530

    Time: 3 ms (52%), Space: 32.9 MB (5.02%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,13 +1,67 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
+        vector<int>neg,pos;
+
         for(int i=0;i<nums.size();i++){
-            nums[i]=nums[i]*nums[i];
+            if(nums[i]>=0){
+                pos.push_back(nums[i]);
+            }
+            else{
+                neg.push_back(nums[i]);
+            }
         }
 
-        sort(nums.begin(),nums.end());
+        if(neg.empty()){
+            for(int i=0;i<pos.size();i++){
+                pos[i]=pos[i]*pos[i];
+            }
+            return pos;
+        }
 
-        return nums;
+        if(pos.empty()){
+            for(int i=0;i<neg.size();i++){
+                neg[i]=neg[i]*neg[i];
+            }
+            reverse(neg.begin(),neg.end());
+            return neg;
+        }
+
+        for(int i=0;i<pos.size();i++){
+            pos[i]=pos[i]*pos[i];
+        }
+
+        for(int i=0;i<neg.size();i++){
+            neg[i]=neg[i]*neg[i];
+        }
+        
+        reverse(neg.begin(),neg.end());
+
+        int a=0,b=0;
+        vector<int>res;
+
+        while(a<neg.size() && b<pos.size()){
+            if(neg[a]<pos[b]){
+                res.push_back(neg[a]);
+                a++;
+            }
+            else{
+                res.push_back(pos[b]);
+                b++;
+            }
+        }
+
+        while(b<pos.size()){
+            res.push_back(pos[b]);
+            b++;
+        }
+
+        while(a<neg.size()){
+            res.push_back(neg[a]);
+            a++;
+        } 
+
+        return res;
+
     }
 };
