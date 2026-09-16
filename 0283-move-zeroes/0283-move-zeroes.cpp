e5855f08a12ab5ee49052e4c28ae0697afed017c
NAMAN GOEL <goelnaman366@gmail.com>
2026-09-16 06:27:37 +0530

    Time: 0 ms (100%), Space: 25 MB (5.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int index=0;
+        vector<int> ans,end;
 
-        for(int i=0;i<nums.size();++i){
-            if(nums[i]!=0){
-                if(i != index){
-                    int temp=nums[index];
-                    nums[index]=nums[i];
-                    nums[i]=temp;
-                }
-                index++;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                ans.push_back(nums[i]);
             }
+            else{
+                end.push_back(nums[i]);
+            }
+
+        }
+        int i=0,j=0,index=0;
+
+        while(i<ans.size()){
+            nums[index]=ans[i];
+            i++;
+            index++;
+        }
+
+        while(j<end.size()){
+            nums[index]=end[j];
+            j++;
+            index++;
         }
     }
 };
