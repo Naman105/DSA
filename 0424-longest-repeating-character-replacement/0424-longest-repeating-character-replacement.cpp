80406645bd66b5886480f96097e1c65dc0c950d8
NAMAN GOEL <goelnaman366@gmail.com>
2026-09-23 08:44:53 +0530

    Time: 3 ms (67.01%), Space: 11 MB (18.77%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int freq[256] = {0};
-        int low=0,res=INT_MIN;
+        int low=0,res=INT_MIN,maxFreq=0;
 
         for(int high=0;high<s.size();high++){
             freq[s[high]]++;
             int len=high-low+1;
-            int maxFreq=*max_element(freq,freq+256);
+            maxFreq=max(maxFreq,freq[s[high]]);
             int diff= len- maxFreq;
 
             while(diff>k){
                 freq[s[low]]--;
                 low++;
                 len=high-low+1;
-                maxFreq=*max_element(freq,freq+256);
+                maxFreq=max(maxFreq,freq[s[high]]);
                 diff=len-maxFreq;
             }
 
